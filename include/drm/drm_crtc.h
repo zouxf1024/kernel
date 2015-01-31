32d55317211eeef40d62ad8becc594f12f8afbc4
Heiko Stuebner <heiko@sntech.de>
2015-01-31 17:32:54 +0100

    drm/encoder: allow encoders to remember their of_node
    Add an of_node field to struct drm_encoder to let encoders optionally
    remember from which devicetree node they originated.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- include/drm/drm_crtc.h
+++ include/drm/drm_crtc.h
@@ -569,6 +569,7 @@ struct drm_encoder_funcs {
 /**
  * struct drm_encoder - central DRM encoder structure
  * @dev: parent DRM device
+ * @of_node: device node pointer to the bridge
  * @head: list management
  * @base: base KMS object
  * @name: encoder name
@@ -585,6 +586,9 @@ struct drm_encoder_funcs {
  */
 struct drm_encoder {
 	struct drm_device *dev;
+#ifdef CONFIG_OF
+	struct device_node *of_node;
+#endif
 	struct list_head head;
 
 	struct drm_mode_object base;
