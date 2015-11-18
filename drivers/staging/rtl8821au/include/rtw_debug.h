81e9b4f062caf48caaedebd2fe736482e7173f78
Xing Zheng <zhengxing@rock-chips.com>
2015-11-18 17:29:13 +0800

    DEVELOP: rtl8821au: Remove a lot of debug infos
    Signed-off-by: Xing Zheng <zhengxing@rock-chips.com>

--- drivers/staging/rtl8821au/include/rtw_debug.h
+++ drivers/staging/rtl8821au/include/rtw_debug.h
@@ -183,8 +183,10 @@ extern void rtl871x_cedbg(const char *fmt, ...);
 	#define _dbgdump rtl871x_cedbg
 	#define _seqdump(sel, fmt, arg...) _dbgdump(fmt, ##arg)
 #elif defined PLATFORM_LINUX
-	#define _dbgdump printk
-	#define _seqdump seq_printf
+	// #define _dbgdump printk
+	// #define _seqdump seq_printf
+	#define _dbgdump
+	#define _seqdump
 #elif defined PLATFORM_FREEBSD
 	#define _dbgdump printf
 	#define _seqdump(sel, fmt, arg...) _dbgdump(fmt, ##arg)
