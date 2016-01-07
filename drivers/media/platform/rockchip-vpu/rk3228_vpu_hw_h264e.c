33804faa410dbdea9978003f5fd38a83e5bf984e
alpha lin <alpha.lin@rock-chips.com>
2016-01-07 15:39:11 +0800

    rk3228/vpu: write direct motion vector quarter pixel penalty table to register
    Signed-off-by: alpha lin <alpha.lin@rock-chips.com>

--- drivers/media/platform/rockchip-vpu/rk3228_vpu_hw_h264e.c
+++ drivers/media/platform/rockchip-vpu/rk3228_vpu_hw_h264e.c
@@ -763,14 +763,14 @@ static inline unsigned int ref_luma_size(unsigned int w, unsigned int h)
 static void write_ue(struct stream_s *fifo, u32 val,
 				  const char *name)
 {
-	u32 numBits = 0;
+	u32 num_bits = 0;
 
 	val++;
-	while(val >> ++numBits);
+	while(val >> ++num_bits);
 
-	if (numBits > 12) {
+	if (num_bits > 12) {
 		u32 tmp;
-		tmp = numBits-1;
+		tmp = num_bits-1;
 
 		if (tmp > 24) {
 			tmp -= 24;
@@ -779,15 +779,15 @@ static void write_ue(struct stream_s *fifo, u32 val,
 
 		stream_put_bits(fifo, 0, tmp, name);
 
-		if (numBits > 24) {
-			numBits -= 24;
-			stream_put_bits(fifo, val >> numBits, 24, name);
-			val = val >> numBits;
+		if (num_bits > 24) {
+			num_bits -= 24;
+			stream_put_bits(fifo, val >> num_bits, 24, name);
+			val = val >> num_bits;
 		}
 
-		stream_put_bits(fifo, val, numBits, name);
+		stream_put_bits(fifo, val, num_bits, name);
 	} else {
-		stream_put_bits(fifo, val , 2 * numBits - 1, name);
+		stream_put_bits(fifo, val , 2 * num_bits - 1, name);
 	}
 }
 
@@ -836,10 +836,10 @@ static int rk3228_vpu_h264e_assumble_sps(struct rockchip_vpu_ctx *ctx)
 	stream_put_bits(sps, 0, 1, "gaps_in_frame_num_value_allowed_flag");
 
 	write_ue(sps, MB_WIDTH(ctx->src_fmt.width) - 1,
-			      "pic_width_in_mbs_minus1");
+			       "pic_width_in_mbs_minus1");
 
 	write_ue(sps, MB_HEIGHT(ctx->src_fmt.height) - 1,
-			      "pic_height_in_map_units_minus1");
+				"pic_height_in_map_units_minus1");
 
 	stream_put_bits(sps, 1, 1, "frame_mbs_only_flag");
 
@@ -1172,12 +1172,9 @@ static void rk3228_vpu_h264e_set_params(struct rockchip_vpu_dev *vpu,
 	vepu_write_relaxed(vpu, 0, VEPU_REG_RGB2YUV_CONVERSION_COEF3);
 	vepu_write_relaxed(vpu, 0, VEPU_REG_RGB_MASK_MSB);
 
-	diff_mv_penalty[0] =
-		h264_diff_mv_penalty4p[params->qp];
-	diff_mv_penalty[1] =
-		h264_diff_mv_penalty[params->qp];
-	diff_mv_penalty[2] =
-		h264_diff_mv_penalty[params->qp];
+	diff_mv_penalty[0] = h264_diff_mv_penalty4p[params->qp];
+	diff_mv_penalty[1] = h264_diff_mv_penalty[params->qp];
+	diff_mv_penalty[2] = h264_diff_mv_penalty[params->qp];
 	split_penalty[0] = 0;
 	split_penalty[1] = 0;
 	split_penalty[2] = 0;
@@ -1226,14 +1223,17 @@ static void rk3228_vpu_h264e_set_params(struct rockchip_vpu_dev *vpu,
 		reg |= VEPU_REG_DMV_PENALTY_TABLE_BIT(dmv_penalty[i + 2], 1);
 		reg |= VEPU_REG_DMV_PENALTY_TABLE_BIT(dmv_penalty[i + 3], 0);
 		vepu_write_relaxed(vpu, reg, VEPU_REG_DMV_PENALTY_TBL(i / 4));
-	}
 
-	for (i = 0; i < 128; i += 4) {
-		reg = VEPU_REG_DMV_PENALTY_TABLE_BIT(dmv_penalty[i], 3);
-		reg |= VEPU_REG_DMV_PENALTY_TABLE_BIT(dmv_penalty[i + 1], 2);
-		reg |= VEPU_REG_DMV_PENALTY_TABLE_BIT(dmv_penalty[i + 2], 1);
-		reg |= VEPU_REG_DMV_PENALTY_TABLE_BIT(dmv_penalty[i + 3], 0);
-		vepu_write_relaxed(vpu, reg, VEPU_REG_DMV_PENALTY_TBL(i / 4));
+		reg = VEPU_REG_DMV_Q_PIXEL_PENALTY_TABLE_BIT(
+			dmv_qpel_penalty[i], 3);
+		reg |= VEPU_REG_DMV_Q_PIXEL_PENALTY_TABLE_BIT(
+			dmv_qpel_penalty[i + 1], 2);
+		reg |= VEPU_REG_DMV_Q_PIXEL_PENALTY_TABLE_BIT(
+			dmv_qpel_penalty[i + 2], 1);
+		reg |= VEPU_REG_DMV_Q_PIXEL_PENALTY_TABLE_BIT(
+			dmv_qpel_penalty[i + 3], 0);
+		vepu_write_relaxed(vpu, reg,
+				   VEPU_REG_DMV_Q_PIXEL_PENALTY_TBL(i / 4));
 	}
 }
 
